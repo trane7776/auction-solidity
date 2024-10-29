37e95325a72e76e15e735b858e7554210a9a925e
trane7776 <izbasarov.t@edu.rea.ru>
2024-10-29 23:32:37 +0300

    lesson 12

---
+++ test/Demo.test.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('AucEngine', () => {
+  let owner;
+  let demo;
+
+  beforeEach(async function () {
+    [owner] = await ethers.getSigners();
+
+    const Logger = await ethers.getContractFactory('Logger', owner);
+    const logger = await Logger.deploy();
+
+    await logger.waitForDeployment();
+
+    const Demo = await ethers.getContractFactory('Demo', owner);
+    demo = await Demo.deploy(logger.target);
+
+    await demo.waitForDeployment();
+  });
+
+  it('allows to pay and get payment info', async function () {
+    const sum = 100;
+    const txData = {
+      value: sum,
+      to: demo.target,
+    };
+
+    const tx = await owner.sendTransaction(txData);
+    await tx.wait();
+
+    await expect(tx).to.changeEtherBalance(demo, sum);
+
+    const amount = await demo.payment(owner.address, 0);
+
+    expect(amount).to.equal(sum);
+  });
+});
